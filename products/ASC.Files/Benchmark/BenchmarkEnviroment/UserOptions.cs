fbfb5611c2df6edd4a4a182be21941bdcc9d1f85
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-04 21:44:46 +0300

    Initial commit

---
+++ products/ASC.Files/Benchmark/BenchmarkEnviroment/UserOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Files.Benchmark
+{
+    public class UserOptions
+    {
+        public const string User = "User";
+        public Guid ParentId { get; set; }
+        public Guid Id { get; set; }
+        public int TenantId { get; set; }
+    }
+}
---
+++ products/ASC.Files/Benchmark/TestsConfiguration/MyDocumentsConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Toolchains.InProcess;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark
+{
+    public class MyDocumentsConfig : ManualConfig
+    {
+        public MyDocumentsConfig()
+        {
+            AddJob(Job.Default
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(100))
+                .AddColumn(StatisticColumn.P0)
+                .AddColumn(StatisticColumn.P25)
+                .AddColumn(StatisticColumn.P50)
+                .AddColumn(StatisticColumn.P67)
+                .AddColumn(StatisticColumn.P80)
+                .AddColumn(StatisticColumn.P85)
+                .AddColumn(StatisticColumn.P90)
+                .AddColumn(StatisticColumn.P95)
+                .AddColumn(StatisticColumn.P100);
+        }
+    }
+}
