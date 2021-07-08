0e50267715d4917816851e98a14ebdaa398bb335
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-08 12:41:35 +0300

    GetFileInfoTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/GetFileInfoTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class GetFileInfoTestConfig : ConfigurationBase
+    {
+        public GetFileInfoTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["GetFileInfoTest:IterationCount"])));
+        }
+    }
+}
