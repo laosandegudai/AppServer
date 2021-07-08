1a91cbec3735f33c07fbfd767e5b0fc182347754
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-08 12:39:21 +0300

    add GetFileInfoBenchmark

---
+++ products/ASC.Files/Benchmark/Benchmarks/GetFileInfoBenchmark.cs
@@ -0,0 +1,27 @@
+﻿
+using ASC.Files.Benchmark.TestsConfiguration;
+
+using BenchmarkDotNet.Attributes;
+
+namespace ASC.Files.Benchmark
+{
+    [Config(typeof(GetFileInfoTestConfig))]
+    public class GetFileInfoBenchmark
+    {
+        private BenchmarkFilesSut _sut = new BenchmarkFilesSut();
+        private int _fileId;
+
+        [GlobalSetup(Target = nameof(GetFileInfoTest))]
+        public void GlobalSetupGetFileInfo()
+        {
+            _sut.Setup();
+            _fileId = _sut.CreateFile();
+        }
+
+        [Benchmark]
+        public void GetFileInfoTest()
+        {
+            _sut.GetFileInfo(_fileId);
+        }
+    }
+}
