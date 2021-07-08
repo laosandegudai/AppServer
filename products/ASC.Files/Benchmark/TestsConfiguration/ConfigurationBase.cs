d22ad3b032dec67843af2c97200daf761b66a7c2
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-08 12:40:14 +0300

    add ConfigurationBase

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/ConfigurationBase.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using BenchmarkDotNet.Configs;
+
+using Microsoft.Extensions.Configuration;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class ConfigurationBase : ManualConfig
+    {
+        protected IConfigurationRoot appConfig = new ConfigurationBuilder()
+                .AddJsonFile(Path.GetFullPath("benchmarksettings.json"), false, false)
+                .Build();
+    }
+}
