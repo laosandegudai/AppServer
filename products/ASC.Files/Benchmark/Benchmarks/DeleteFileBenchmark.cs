4365c838e98936af9966698d778a0ed3aa43fc27
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-08 12:39:08 +0300

    add DeleteFileBenchmark

---
+++ products/ASC.Files/Benchmark/Benchmarks/DeleteFileBenchmark.cs
@@ -0,0 +1,27 @@
+﻿
+using ASC.Files.Benchmark.TestsConfiguration;
+
+using BenchmarkDotNet.Attributes;
+
+namespace ASC.Files.Benchmark
+{
+    [Config(typeof(DeleteFileTestConfig))]
+    public class DeleteFileBenchmark
+    {
+        private BenchmarkFilesSut _sut = new BenchmarkFilesSut();
+        private int _fileId;
+
+        [IterationSetup(Target = nameof(DeleteFileTest))]
+        public void IterSetupDeleteFile()
+        {
+            _sut.Setup();
+            _fileId = _sut.CreateFile();
+        }
+
+        [Benchmark]
+        public void DeleteFileTest()
+        {
+            _sut.DeleteFile(_fileId);
+        }
+    }
+}
