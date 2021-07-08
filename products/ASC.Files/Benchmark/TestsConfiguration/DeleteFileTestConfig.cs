91a37be3b5d629429a2c9f09546e2a374835022e
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-08 12:40:54 +0300

    add DeleteFileTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/DeleteFileTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class DeleteFileTestConfig : ConfigurationBase
+    {
+        public DeleteFileTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["DeleteFileTest:IterationCount"])));
+        }
+    }
+}
