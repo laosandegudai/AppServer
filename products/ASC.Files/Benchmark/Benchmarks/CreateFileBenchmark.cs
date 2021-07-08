d55b88bb1e6e668f806857f8335624c5cd9e87a4
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-08 12:37:26 +0300

    add CreateFileBenchmark

---
+++ products/ASC.Files/Benchmark/Benchmarks/CreateFileBenchmark.cs
@@ -0,0 +1,23 @@
+﻿
+using BenchmarkDotNet.Attributes;
+
+namespace ASC.Files.Benchmark
+{
+    [Config(typeof(CreateFileTestConfig))]
+    public class CreateFileBenchmark
+    {
+        private BenchmarkFilesSut _sut = new BenchmarkFilesSut();
+
+        [IterationSetup(Target = nameof(CreateFileTest))]
+        public void IterSetupCreateFile()
+        {
+            _sut.Setup();
+        }
+
+        [Benchmark]
+        public void CreateFileTest()
+        {
+            _sut.CreateFile();
+        }
+    }
+}
