57e3777743fbe2f83efba7bf885fe6e5d2136b9f
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:35:55 +0300

    Test: Benchmark: add test with users count, cosmetic

--- products/ASC.Files/Benchmark/Benchmarks/CreateFileBenchmark.cs
+++ products/ASC.Files/Benchmark/Benchmarks/CreateFileBenchmark.cs
@@ -1,23 +1,54 @@
-﻿
+﻿using System.Threading.Tasks;
+
+using ASC.Files.Benchmark.BenchmarkEnviroment;
+using ASC.Files.Benchmark.TestsConfiguration;
+
 using BenchmarkDotNet.Attributes;
 
-namespace ASC.Files.Benchmark
+namespace ASC.Files.Benchmark.Benchmarks
 {
     [Config(typeof(CreateFileTestConfig))]
     public class CreateFileBenchmark
     {
-        private BenchmarkFilesSut _sut = new BenchmarkFilesSut();
+        private TestDataStorage _usersStorage = TestDataStorage.GetStorage();
+        private Task[] _tasks;
 
-        [IterationSetup(Target = nameof(CreateFileTest))]
-        public void IterSetupCreateFile()
-        {
-            _sut.Setup();
-        }
-
+        #region CreateFileTest
         [Benchmark]
         public void CreateFileTest()
         {
-            _sut.CreateFile();
+            _usersStorage.Users[0].CreateFileInMy();
         }
+        #endregion
+
+        #region CreateFileUsersTest
+        [IterationSetup(Target = nameof(CreateFileUsersTest))]
+        public void IterSetupCreateFileUsersTest()
+        {
+
+            _tasks = new Task[_usersStorage.Users.Count];
+
+            for (int i = 0; i < _usersStorage.Users.Count; i++)
+            {
+                var user = _usersStorage.Users[i];
+
+                _tasks[i] = new Task(() =>
+                {
+                    user.CreateFileInMy();
+                });
+            }
+        }
+
+        [Benchmark]
+        public void CreateFileUsersTest()
+        {
+            foreach (var task in _tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(_tasks);
+        }
+        #endregion
     }
 }
