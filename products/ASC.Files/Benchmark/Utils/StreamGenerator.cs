c65b5ad9003dbcaac7a71b0760c436f0362f358d
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:48:52 +0300

    Test: Benchmark: move StreamGenerator, TestStream

---
+++ products/ASC.Files/Benchmark/Utils/StreamGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ASC.Files.Benchmark.Utils
+{
+    public static class StreamGenerator
+    {
+        public static TestStream Generate(int bytesCount)
+        {
+            var bytes = Encoding.UTF8.GetBytes("test");
+            var buffer = new byte[bytesCount];
+
+            var stream = new MemoryStream(buffer);
+
+            while (stream.Position < stream.Length)
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+
+            stream.Position = 0;
+
+            return new TestStream(stream);
+        }
+    }
+}
