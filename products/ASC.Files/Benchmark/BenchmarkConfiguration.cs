43f354c320c7081349b51e9748e444e864262b50
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:54:58 +0300

    Test: Benchmark: add BenchmarkConfiguration

---
+++ products/ASC.Files/Benchmark/BenchmarkConfiguration.cs
@@ -0,0 +1,29 @@
+﻿
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+
+namespace ASC.Files.Benchmark
+{
+    public class BenchmarkConfiguration
+    {
+        public IConfigurationRoot Config { get; private set; }
+
+        private static BenchmarkConfiguration _instance;
+
+        private BenchmarkConfiguration()
+        {
+            Config = new ConfigurationBuilder()
+                .AddJsonFile(Path.GetFullPath("benchmarksettings.json"), false, false)
+                .Build();
+        }
+
+        public static BenchmarkConfiguration Build()
+        {
+            if (_instance == null)
+                _instance = new BenchmarkConfiguration();
+
+            return _instance;
+        }
+    }
+}
