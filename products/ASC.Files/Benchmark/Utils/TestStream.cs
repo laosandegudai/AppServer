22213d2e45bc0473720d262ac3ba61f4a6549d51
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:48:52 +0300

    Test: Benchmark: move StreamGenerator, TestStream

---
+++ products/ASC.Files/Benchmark/Utils/TestStream.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace ASC.Files.Benchmark.Utils
+{
+    public class TestStream
+    {
+        public Stream Stream { get; }
+
+        public TestStream(Stream stream)
+        {
+            Stream = stream;
+        }
+
+        public override string ToString()
+        {
+            return $"{Stream.Length / 1024} KB";
+        }
+    }
+}
