31ca5f8747fd430cc28bf951e46a7bf7e19fcd84
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:46:07 +0300

    Test: Benchmark: add test with users count, cosmetic

--- products/ASC.Files/Benchmark/Benchmarks/UpdateFileStreamBenchmark.cs
+++ products/ASC.Files/Benchmark/Benchmarks/UpdateFileStreamBenchmark.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 using ASC.Files.Benchmark.BenchmarkEnviroment;
 using ASC.Files.Benchmark.TestsConfiguration;
+using ASC.Files.Benchmark.Utils;
 
 using BenchmarkDotNet.Attributes;
 
-namespace ASC.Files.Benchmark
+namespace ASC.Files.Benchmark.Benchmarks
 {
     [Config(typeof(UpdateFileStreamTestConfig))]
     public class UpdateFileStreamBenchmark
     {
-        private BenchmarkFilesSut _sut = new BenchmarkFilesSut();
+        private TestDataStorage _usersStorage = TestDataStorage.GetStorage();
         private int _fileId;
+        private List<int> _filesId = new List<int>();
+        private Task[] _tasks;
+        private List<TestStream> _testStreams = new List<TestStream>();
 
         public IEnumerable<TestStream> TestStreams()
         {
@@ -21,16 +27,9 @@ namespace ASC.Files.Benchmark
         }
 
         [GlobalSetup(Target = nameof(UpdateFileStreamTest))]
-        public void SetupUpdateFileStream()
+        public void IterSetupUpdateFileStreamTest()
         {
-            _sut.Setup();
-            _fileId = _sut.CreateFile();
-        }
-
-        [IterationSetup(Target = nameof(UpdateFileStreamTest))]
-        public void IterSetupUpdateFileStream()
-        {
-            _sut.Setup();
+            _fileId = _usersStorage.Users[0].CreateFileInMy();
         }
 
         [Benchmark]
@@ -38,7 +37,58 @@ namespace ASC.Files.Benchmark
         public void UpdateFileStreamTest(TestStream testStream)
         {
             testStream.Stream.Position = 0;
-            _sut.UpdateFileStream(_fileId, testStream.Stream);
+            _usersStorage.Users[0].UpdateFileStream(_fileId, testStream.Stream);
+        }
+
+        [GlobalSetup(Target = nameof(UpdateFileStreamUsersTest))]
+        public void GlobalSetupUpdateFileStreamUsersTest()
+        {
+            foreach (var user in _usersStorage.Users)
+            {
+                _filesId.Add(user.CreateFileInMy());
+            }
+
+            var testStream = StreamGenerator.Generate(10240);
+
+            for (int i = 0; i < _usersStorage.Users.Count; i++)
+            {
+                testStream.Stream.Position = 0;
+                var memoryStream = new MemoryStream();
+                testStream.Stream.CopyTo(new MemoryStream());
+
+                _testStreams.Add(new TestStream(memoryStream));
+            }
+        }
+
+        [IterationSetup(Target = nameof(UpdateFileStreamUsersTest))]
+        public void IterSetupUpdateFileStreamUsersTest()
+        {
+            _tasks = new Task[_usersStorage.Users.Count];
+
+            for (int i = 0; i < _usersStorage.Users.Count; i++)
+            {
+                var user = _usersStorage.Users[i];
+                var fileId = _filesId[i];
+                var testStream = _testStreams[i];
+
+                testStream.Stream.Position = 0;
+
+                _tasks[i] = new Task(() =>
+                {
+                    user.UpdateFileStream(fileId, testStream.Stream);
+                });
+            }
+        }
+
+        [Benchmark]
+        public void UpdateFileStreamUsersTest()
+        {
+            foreach (var task in _tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(_tasks);
         }
     }
 }
