88663276529be29fc5c5c8d647c4a4ddcf4f1fe1
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:20:15 +0300

    Test: Benchmark: change namespace

--- products/ASC.Files/Benchmark/BenchmarkEnviroment/UserOptions.cs
+++ products/ASC.Files/Benchmark/BenchmarkEnviroment/UserOptions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ASC.Files.Benchmark
+namespace ASC.Files.Benchmark.BenchmarkEnviroment
 {
     public class UserOptions
     {
