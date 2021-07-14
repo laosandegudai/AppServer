146e37b9f377608c85c7e02bb2f17fdf0b8c90dd
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:51:13 +0300

    Test: Benchmark: add CreateFolderTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/CreateFolderTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class CreateFolderTestConfig : ConfigurationBase
+    {
+        public CreateFolderTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["CreateFolderTest:IterationCount"])));
+        }
+    }
+}
