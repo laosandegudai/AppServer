46fcc3722ce9b55d8a9637b774c0d500baad36cb
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:23:19 +0300

    Test: Benchmark: add BenchmarkFilesHost

---
+++ products/ASC.Files/Benchmark/BenchmarkEnviroment/BenchmarkFilesHost.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+using Microsoft.Extensions.Hosting;
+
+namespace ASC.Files.Benchmark.BenchmarkEnviroment
+{
+    public class BenchmarkFilesHost
+    {
+        public IHost Host { get; }
+
+        private const string TestConnection = "Server=localhost;Database=onlyoffice_benchmark;User ID=root;Password=onlyoffice;Pooling=true;Character Set=utf8;AutoEnlist=false;SSL Mode=none;AllowPublicKeyRetrieval=True;";
+
+        public BenchmarkFilesHost()
+        {
+            Host = Program.CreateHostBuilder(new string[] {
+                "--pathToConf" , Path.Combine("..", "..", "..", "..", "..", "config"),
+                "--ConnectionStrings:default:connectionString", TestConnection,
+                 "--migration:enabled", "true"}).Build();
+        }
+    }
+}
