bc0b758f6daffcb407c5cee2179e9b6caf7d7d24
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:28:18 +0300

    Test: Benchmark: add BenchmarkTestEnviroment

---
+++ products/ASC.Files/Benchmark/BenchmarkEnviroment/BenchmarkTestEnviroment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ASC.Files.Benchmark.BenchmarkEnviroment
+{
+    public class BenchmarkTestEnviroment : IDisposable
+    {
+        private BenchmarkDb db = new BenchmarkDb();
+
+        public void Create()
+        {
+            db.Create();
+            var admin = new TestUser(new BenchmarkFilesHost(), true);
+            admin.CreateFileInMy();
+            admin.CreateCommonFolder();
+        }
+
+        public void Drop()
+        {
+            db.Drop();
+        }
+
+        public void Dispose()
+        {
+            Drop();
+        }
+    }
+}
