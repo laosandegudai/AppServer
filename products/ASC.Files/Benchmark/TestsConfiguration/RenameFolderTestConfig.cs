faabfc183174d663798db426aae016439dc0a9b1
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:53:44 +0300

    Test: Benchmark: add RenameFolderTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/RenameFolderTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class RenameFolderTestConfig : ConfigurationBase
+    {
+        public RenameFolderTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["RenameFolderTest:IterationCount"])));
+        }
+    }
+}
