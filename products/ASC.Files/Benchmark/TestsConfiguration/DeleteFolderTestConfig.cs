6474eaa4a9e0cace915d6bc9a957450d453eaa28
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:52:36 +0300

    Test: Benchmark: add DeleteFolderTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/DeleteFolderTestConfig.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class DeleteFolderTestConfig : ConfigurationBase
+    {
+        public DeleteFolderTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["DeleteFolderTest:IterationCount"])));
+        }
+    }
+}
