a4a6378c06fb180fba986296c66c6e08424ca3df
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:37:40 +0300

    Test: Benchmark: add test with users count, cosmetic

--- products/ASC.Files/Benchmark/Benchmarks/DeleteFileBenchmark.cs
+++ products/ASC.Files/Benchmark/Benchmarks/DeleteFileBenchmark.cs
@@ -1,27 +1,71 @@
 ﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using ASC.Files.Benchmark.BenchmarkEnviroment;
 using ASC.Files.Benchmark.TestsConfiguration;
 
 using BenchmarkDotNet.Attributes;
 
-namespace ASC.Files.Benchmark
+namespace ASC.Files.Benchmark.Benchmarks
 {
     [Config(typeof(DeleteFileTestConfig))]
     public class DeleteFileBenchmark
     {
-        private BenchmarkFilesSut _sut = new BenchmarkFilesSut();
+        private TestDataStorage _usersStorage = TestDataStorage.GetStorage();
         private int _fileId;
+        private List<int> _filesId;
+        private Task[] _tasks;
 
+        #region DeleteFileTest
         [IterationSetup(Target = nameof(DeleteFileTest))]
-        public void IterSetupDeleteFile()
+        public void IterSetupDeleteFileTest()
         {
-            _sut.Setup();
-            _fileId = _sut.CreateFile();
+            _fileId = _usersStorage.Users[0].CreateFileInMy();
         }
-
+        
         [Benchmark]
         public void DeleteFileTest()
         {
-            _sut.DeleteFile(_fileId);
+            _usersStorage.Users[0].DeleteFile(_fileId);
         }
+        #endregion
+
+        #region DeleteFileUsersTest
+        [IterationSetup(Target =nameof(DeleteFileUsersTest))]
+        public void IterSetupDeleteFileUsersTest()
+        {
+            _filesId = new List<int>();
+
+            foreach (var user in _usersStorage.Users)
+            {
+                _filesId.Add(user.CreateFileInMy());
+            }
+
+            _tasks = new Task[_usersStorage.Users.Count];
+
+            for (int i = 0; i < _usersStorage.Users.Count; i++)
+            {
+                var user = _usersStorage.Users[i];
+                var fileId = _filesId[i];
+
+                _tasks[i] = new Task(() =>
+                {
+                    user.DeleteFile(fileId);
+                });
+            }
+        }
+
+        [Benchmark]
+        public void DeleteFileUsersTest()
+        {
+            foreach (var task in _tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(_tasks);
+        }
+        #endregion
     }
 }
