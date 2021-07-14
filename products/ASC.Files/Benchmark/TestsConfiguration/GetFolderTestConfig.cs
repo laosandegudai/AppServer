c80b44d2ee9549fb208c36117cc7444144e25c73
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-14 12:53:13 +0300

    Test: Benchmark: add GetFolderTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/GetFolderTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class GetFolderTestConfig : ConfigurationBase
+    {
+        public GetFolderTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["GetFolderTest:IterationCount"])));
+        }
+    }
+}
