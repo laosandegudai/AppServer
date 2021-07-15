8cc9d005d7dde92c717aca8e2eac5cc09abec4f8
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-15 13:41:21 +0300

    Test: Benchmark: add AddToFavoritesTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/AddToFavoritesTestConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class AddToFavoritesTestConfig : ConfigurationBase
+    {
+        public AddToFavoritesTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["AddToFavorites:IterationCount"])));
+        }
+    }
+}
