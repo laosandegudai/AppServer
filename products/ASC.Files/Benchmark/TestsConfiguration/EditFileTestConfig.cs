0e5083521bc70189e2951f4565fe21395544321e
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-15 16:07:02 +0300

    Test: Benchmark: add EditFileTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/EditFileTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class EditFileTestConfig : ConfigurationBase
+    {
+        public EditFileTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["EditFileTest:IterationCount"])));
+        }
+    }
+}
