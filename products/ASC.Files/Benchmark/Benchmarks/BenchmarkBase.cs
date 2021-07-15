a2603e868a0c3107ef2bead65d30f8d4f9eacb92
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-15 17:07:32 +0300

    Test: Benchmark: add BenchmarkBase

---
+++ products/ASC.Files/Benchmark/Benchmarks/BenchmarkBase.cs
@@ -0,0 +1,12 @@
+﻿using ASC.Files.Benchmark.BenchmarkEnviroment;
+
+using Microsoft.Extensions.Configuration;
+
+namespace ASC.Files.Benchmark.Benchmarks
+{
+    public class BenchmarkBase
+    {
+        protected IConfigurationRoot _config  = BenchmarkConfiguration.Build().Config;
+        protected TestDataStorage _dataStorage = TestDataStorage.GetStorage();
+    }
+}
