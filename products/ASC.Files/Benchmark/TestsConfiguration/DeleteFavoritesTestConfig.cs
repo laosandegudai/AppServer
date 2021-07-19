c69a445c91a5769f15341b911d32b7266b33965d
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-19 10:44:50 +0300

    Test: Benchmark: add DeleteFavoritesTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/DeleteFavoritesTestConfig.cs
@@ -0,0 +1,17 @@
+﻿
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class DeleteFavoritesTestConfig : ConfigurationBase
+    {
+        public DeleteFavoritesTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["Common:DeleteFavoritesTest:IterationCount"])));
+        }
+    }
+}
