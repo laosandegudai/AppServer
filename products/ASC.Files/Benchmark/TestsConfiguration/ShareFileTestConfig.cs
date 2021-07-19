68e665ed436e2c48619d3d6469b64a8a2c6b573c
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-19 15:26:04 +0300

    Test: Benchmark: add ShareFileTestConfig

---
+++ products/ASC.Files/Benchmark/TestsConfiguration/ShareFileTestConfig.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace ASC.Files.Benchmark.TestsConfiguration
+{
+    public class ShareFileTestConfig : ConfigurationBase
+    {
+        public ShareFileTestConfig()
+        {
+            AddJob(Job.Default
+                .WithStrategy(BenchmarkDotNet.Engines.RunStrategy.Monitoring)
+                .WithToolchain(InProcessEmitToolchain.Instance)
+                .WithIterationCount(int.Parse(appConfig["Files:ShareFileTest:IterationCount"])));
+        }
+    }
+}
