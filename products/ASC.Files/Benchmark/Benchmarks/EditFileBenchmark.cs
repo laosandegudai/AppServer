2216dbdf15f369a3c6a969a09b7d6f92a5005874
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2021-07-20 12:43:20 +0300

    Test: Benchmark: change parameterization

--- products/ASC.Files/Benchmark/Benchmarks/EditFileBenchmark.cs
+++ products/ASC.Files/Benchmark/Benchmarks/EditFileBenchmark.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 using ASC.Files.Benchmark.TestsConfiguration;
+using ASC.Files.Benchmark.Utils;
 
 using BenchmarkDotNet.Attributes;
 
@@ -13,20 +15,31 @@ namespace ASC.Files.Benchmark.Benchmarks
         private int _fileId;
         private List<int> _filesId = new List<int>();
         private Task[] _tasks;
+        private Stream _stream;
+        private List<Stream> _streams = new List<Stream>();
+        private int _bytesCount;
+
+        public EditFileBenchmark()
+        {
+            _bytesCount = int.Parse(_config["Files:EditFileTest:StreamSizeKB"]) * 1024;
+        }
 
         #region EditFileTest
         [GlobalSetup(Target = nameof(EditFileTest))]
         public void GlobalSetupEditFileTest()
         {
             _fileId = _dataStorage.Users[0].CreateFileInMy();
+            _stream = StreamGenerator.Generate(_bytesCount);
         }
 
         [Benchmark]
         public void EditFileTest()
         {
+            _stream.Position = 0;
+
             _dataStorage.Users[0].OpenEdit(_fileId);
             _dataStorage.Users[0].StartEdit(_fileId);
-            _dataStorage.Users[0].SaveEditing(_fileId);
+            _dataStorage.Users[0].SaveEditing(_fileId, _stream);
         }
         #endregion
 
@@ -34,9 +47,17 @@ namespace ASC.Files.Benchmark.Benchmarks
         [GlobalSetup(Target =nameof(EditFileManyUsersTest))]
         public void GlobalSetupEditFileManyUsersTest()
         {
+            var templateStream = StreamGenerator.Generate(_bytesCount);
+
             foreach (var user in _dataStorage.Users)
             {
                 _filesId.Add(user.CreateFileInMy());
+
+                templateStream.Position = 0;
+                var memStream = new MemoryStream();
+                templateStream.CopyTo(memStream);
+
+                _streams.Add(memStream);
             }
         }
 
@@ -49,12 +70,15 @@ namespace ASC.Files.Benchmark.Benchmarks
             {
                 var user = _dataStorage.Users[i];
                 var fileId = _filesId[i];
+                var stream = _streams[i];
+
+                stream.Position = 0;
 
                 _tasks[i] = new Task(() =>
                 {
                     user.OpenEdit(fileId);
                     user.StartEdit(fileId);
-                    user.SaveEditing(fileId);
+                    user.SaveEditing(fileId, stream);
                 }, TaskCreationOptions.LongRunning);
                 _tasks[i].ConfigureAwait(false);
             }
