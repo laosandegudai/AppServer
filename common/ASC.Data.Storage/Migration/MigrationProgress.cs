378e8e5fcda1f8220b9ca1997529405188148b37
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:49:16 +0300

    add custom serialization

---
+++ common/ASC.Data.Storage/Migration/MigrationProgress.cs
@@ -0,0 +1,11 @@
+﻿
+using ASC.Common.Caching;
+
+namespace ASC.Migration
+{
+    public partial class MigrationProgress : ICustomSer<MigrationProgress>
+    {
+        public void CustomDeSer() { }
+        public void CustomSer() { }
+    }
+}
