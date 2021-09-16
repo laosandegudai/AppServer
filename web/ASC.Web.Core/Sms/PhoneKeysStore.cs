7a1319e68d4467949bf601a8771c159fa95ee9f0
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:48:03 +0300

    add phonekeys store

---
+++ web/ASC.Web.Core/Sms/PhoneKeysStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ASC.Common.Caching;
+
+namespace ASC.Web.Core.Sms
+{
+    public partial class PhoneKeysStore : ICustomSer<PhoneKeysStore>
+    {
+        public IDictionary<string, DateTime> PhoneKeys { get; private set; } = new Dictionary<string, DateTime>();
+
+        public PhoneKeysStore(IDictionary<string, DateTime> phoneKeys)
+        {
+            PhoneKeys = phoneKeys;
+        }
+
+        public void CustomDeSer()
+        {
+            foreach (var pair in KeysProto)
+            {
+                PhoneKeys.Add(pair.Key, pair.Value.ToDateTime());
+            }
+        }
+
+        public void CustomSer()
+        {
+            KeysProto.Clear();
+
+            foreach (var pair in PhoneKeys)
+            {
+                KeysProto.Add(pair.Key, Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(pair.Value.ToUniversalTime()));
+            }
+        }
+    }
+}
