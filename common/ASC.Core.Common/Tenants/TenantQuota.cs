8ec64b86e65bc9c1ea89604a10aa568c5c02a4d1
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:29:28 +0300

    move decimal value in new class

--- common/ASC.Core.Common/Tenants/TenantQuota.cs
+++ common/ASC.Core.Common/Tenants/TenantQuota.cs
@@ -34,30 +34,6 @@ using ASC.Common.Caching;
 
 namespace ASC.Core.Tenants
 {
-    public partial class DecimalValue
-    {
-        private const decimal NanoFactor = 1_000_000_000;
-
-        public DecimalValue(long units, int nanos)
-        {
-            Units = units;
-            Nanos = nanos;
-        }
-
-        public static implicit operator decimal(DecimalValue grpcDecimal)
-        {
-            return grpcDecimal.Units + grpcDecimal.Nanos / NanoFactor;
-        }
-
-        public static implicit operator DecimalValue(decimal value)
-        {
-            var units = decimal.ToInt64(value);
-            var nanos = decimal.ToInt32((value - units) * NanoFactor);
-
-            return new DecimalValue(units, nanos);
-        }
-    }
-
     public partial class TenantQuotaList : ICustomSer<TenantQuotaList>,
         IEnumerable<TenantQuota>
     {
