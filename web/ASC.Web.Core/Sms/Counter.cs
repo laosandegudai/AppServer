903d19f04d07a4befcbea11d6cff94b0f129e22e
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:48:33 +0300

    add wrapper type

---
+++ web/ASC.Web.Core/Sms/Counter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASC.Common.Caching;
+
+namespace ASC.Web.Core.Sms
+{
+    public partial class Counter : ICustomSer<Counter>
+    {
+        public Counter(string value)
+        {
+            Value = value;
+        }
+
+        public void CustomDeSer() { }
+
+        public void CustomSer() { }
+    }
+}
