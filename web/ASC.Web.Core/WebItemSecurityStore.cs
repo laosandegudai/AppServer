e57fa8d442dcf0ad4eeba6b8b6fc90ebfd7e403a
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:54:21 +0300

    add webitemsecurity store

---
+++ web/ASC.Web.Core/WebItemSecurityStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASC.Common.Caching;
+
+namespace ASC.Web.Core
+{
+    public partial class WebItemSecurityStore : ICustomSer<WebItemSecurityStore>
+    {
+        public void CustomDeSer() { }
+        public void CustomSer() { }
+    }
+}
