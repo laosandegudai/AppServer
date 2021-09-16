96675674d3029964c706d63aa11bd215540d72b5
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:51:44 +0300

    add product price store

---
+++ common/ASC.Core.Common/Billing/ProductPriceInfoStore.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+using ASC.Common.Caching;
+
+namespace ASC.Core.Billing
+{
+    public partial class ProductPriceInfoStore : ICustomSer<ProductPriceInfoStore>
+    {
+        public IDictionary<string, Dictionary<string, decimal>> Infos { get; } =
+            new Dictionary<string, Dictionary<string, decimal>>();
+
+        public ProductPriceInfoStore(IDictionary<string, Dictionary<string, decimal>> dict)
+        {
+            foreach (var pair in dict)
+            {
+                Infos.Add(pair.Key, pair.Value);
+            }
+        }
+
+        public void CustomDeSer()
+        {
+            foreach (var info in ProductPriceInfos)
+            {
+                info.Value.CustomDeSer();
+                Infos.Add(info.Key, info.Value.Infos);
+            }
+        }
+
+        public void CustomSer()
+        {
+            ProductPriceInfos.Clear();
+
+            foreach (var info in Infos)
+            {
+                var priceInfo = new ProductPriceInfo(info.Value);
+                priceInfo.CustomSer();
+                ProductPriceInfos.Add(info.Key, priceInfo);
+            }
+        }
+    }
+
+    public partial class ProductPriceInfo : ICustomSer<ProductPriceInfo>
+    {
+        public Dictionary<string, decimal> Infos { get; } = new Dictionary<string, decimal>();
+
+        public ProductPriceInfo(IDictionary<string, decimal> dict)
+        {
+            foreach (var pair in dict)
+            {
+                Infos.Add(pair.Key, pair.Value);
+            }
+        }
+
+        public void CustomDeSer()
+        {
+            foreach (var pair in PriceInfos)
+            {
+                Infos.Add(pair.Key, (decimal)pair.Value);
+            }
+        }
+
+        public void CustomSer()
+        {
+            PriceInfos.Clear();
+
+            foreach (var pair in Infos)
+            {
+                PriceInfos.Add(pair.Key, (DecimalValue)pair.Value);
+            }
+        }
+    }
+}
