6d46b4bb2cc022f081a1343fd2d64655ffaf519b
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-16 18:47:04 +0300

    add fake type

---
+++ common/ASC.Core.Common/Core/Clean.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASC.Common.Caching;
+
+namespace ASC.Core
+{
+    public partial class Clean : ICustomSer<Clean>
+    {
+        public void CustomDeSer() { }
+
+        public void CustomSer() { }
+    }
+}
