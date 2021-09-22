05061901f33a9f9c161d81fee708fd5caa74edbd
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-22 17:15:17 +0300

    add cached enteties store

---
+++ products/ASC.Files/Core/Core/Thirdparty/CachedEntities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASC.FederatedLogin.Helpers;
+
+namespace ASC.Files.Core.Core.Thirdparty
+{
+    public class CachedEntities
+    {
+        private Dictionary<string, object> cache
+            = new Dictionary<string, object>();
+
+        public T Get<T>(string key)
+        {
+            cache.TryGetValue(key, out var value);
+
+            return (T)value;
+        }
+
+        public void Insert<T>(string key, T data)
+        {
+            if (cache.ContainsKey(key)) cache.Remove(key);
+
+            cache.Add(key, data);
+        }
+    }
+}
